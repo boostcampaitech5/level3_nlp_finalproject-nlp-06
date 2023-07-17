4ce67937cd64916e1e248c57c91ac91afff35568
Gaeun Seo <85860941+gaeun0112@users.noreply.github.com>
2023-07-17 23:52:33 +0900

    feat: L_sentence

--- DensePhrases/densephrases/utils/open_utils.py
+++ DensePhrases/densephrases/utils/open_utils.py
@@ -106,6 +106,7 @@ def load_qa_pairs(data_path, args, q_idx=None, draft_num_examples=100, shuffle=F
     questions = []
     answers = []
     titles = []
+    sentences = []
     data = json.load(open(data_path))['data']
     for data_idx, item in enumerate(data):
         if q_idx is not None:
@@ -119,12 +120,14 @@ def load_qa_pairs(data_path, args, q_idx=None, draft_num_examples=100, shuffle=F
             question = question[max(question.index('[START_ENT]')-300, 0):question.index('[END_ENT]')+300]
         answer = item['answers']
         title = item.get('titles', [''])
+        sentence = item.get('sentence', [''])
         if len(answer) == 0:
             continue
         q_ids.append(q_id)
         questions.append(question)
         answers.append(answer)
         titles.append(title)
+        sentences.append(sentence)
     questions = [query[:-1] if query.endswith('?') else query for query in questions]
     # questions = [query.lower() for query in questions] # force lower query
 
@@ -134,8 +137,9 @@ def load_qa_pairs(data_path, args, q_idx=None, draft_num_examples=100, shuffle=F
 
     if shuffle:
         qa_pairs = list(zip(q_ids, questions, answers, titles))
+        qa_pairs = list(zip(q_ids, questions, answers, titles, sentences))
         random.shuffle(qa_pairs)
-        q_ids, questions, answers, titles = zip(*qa_pairs)
+        q_ids, questions, answers, titles, sentences = zip(*qa_pairs)
         logger.info(f'Shuffling QA pairs')
 
     if args.draft:
@@ -143,6 +147,7 @@ def load_qa_pairs(data_path, args, q_idx=None, draft_num_examples=100, shuffle=F
         questions = np.array(questions)[:draft_num_examples].tolist()
         answers = np.array(answers)[:draft_num_examples].tolist()
         titles = np.array(titles)[:draft_num_examples].tolist()
+        sentences = np.array(sentences)[:draft_num_examples].tolist()
 
     if args.truecase:
         try:
@@ -156,6 +161,6 @@ def load_qa_pairs(data_path, args, q_idx=None, draft_num_examples=100, shuffle=F
             print(e)
 
     logger.info(f'Loading {len(questions)} questions from {data_path}')
-    logger.info(f'Sample Q ({q_ids[0]}): {questions[0]}, A: {answers[0]}, Title: {titles[0]}')
+    logger.info(f'Sample Q ({q_ids[0]}): {questions[0]}, A: {answers[0]}, Title: {titles[0]}, Sentence: {sentences[0]}')
     return q_ids, questions, answers, titles
 
