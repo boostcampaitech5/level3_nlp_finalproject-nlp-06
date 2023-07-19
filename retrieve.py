b2bba00a0fce3d0c1f92422ade83cac23bb3855b
GrapeDiget <alsgh8257@naver.com>
2023-07-19 15:47:04 +0000

    feat: add arguments - unit, topk

--- retrieve.py
+++ retrieve.py
@@ -50,12 +50,12 @@ class Retriever():
                 idx = 0
                 for batch_query in tqdm(queries_batch):
                     # retrieve
-                    result = self.model.search(
-                        batch_query, retrieval_unit=R_UNIT, top_k=TOP_K)
+                    result, rets = self.model.search(
+                        batch_query, retrieval_unit=self.args.unit, top_k=self.args.topk, return_meta=True)
 
                     # write to runfile
                     for i in range(len(result)):
-                        fw.write(f"{qids[idx]}\t{result[i]}\n")
+                        fw.write(f"{qids[idx]}\t{result[i]}\t{rets[i]}\n")
                         idx += 1
 
             return None
@@ -86,6 +86,10 @@ if __name__ == "__main__":
                         help="output runfile name which indluces query id and retrieved collection")
     parser.add_argument('--batch_size', type=int, default=1,
                         help="#query to process with parallel processing")
+    parser.add_argument('--unit', type=str, default='sentence',
+                        help="weight scores for duplicate unit when aggregate")
+    parser.add_argument('--topk', type=int, default=100,
+                        help="weight scores for duplicate unit when aggregate")
 
     args = parser.parse_args()
 
